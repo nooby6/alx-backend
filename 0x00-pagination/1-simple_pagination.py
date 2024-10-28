1e9947acbc197139e18bece69c75ba17fc435823
nooby6 <giftaudrine1@gmail.com>
2024-10-28 21:00:17 +0000

    a method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10.

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Contains a class for interacting with a dataset"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page given the page's index and size"""
+        assert isinstance(page, int) and isinstance(
+            page_size, int
+        ), "Values must be integers"
+        assert page > 0 and page_size > 0, "Values must be greater than zero"
+
+        start, end = index_range(page, page_size)
+
+        data = self.dataset()
+
+        if max(start, end) >= len(data):
+            return []
+
+        return data[start:end]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a start index and an end index for given
+    pagination parameters.
+
+    Args
+        page: the page number(1-indexed)
+        page_size: the number of results per page
+
+    Returns
+        (start, end)
+    """
+    start = page_size * (page - 1)
+    end = page_size * page
+
+    return (start, end)
