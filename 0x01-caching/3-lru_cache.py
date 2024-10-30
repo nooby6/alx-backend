c338789bdc107f5c58ab002511eff63f7b0450dc
nooby6 <giftaudrine1@gmail.com>
2024-10-30 18:12:53 +0300

    Implement LRUCache class with LRU replacement strategy and add put/get methods

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+from base_caching import BaseCaching
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.order = []  # This will keep track of the order of keys for LRU
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        
+        if key in self.cache_data:
+            # Update the existing item and move the key to the end of the order list
+            self.cache_data[key] = item
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            # If the cache is full, discard the least recently used item
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.order.pop(0)  # Get the first key (least recently used)
+                del self.cache_data[lru_key]  # Remove it from the cache
+                print(f"DISCARD: {lru_key}")  # Print the discarded key
+            
+            # Add the new item to the cache
+            self.cache_data[key] = item
+            self.order.append(key)  # Add the new key to the order list
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        
+        # Move the accessed key to the end of the order list to mark it as recently used
+        self.order.remove(key)
+        self.order.append(key)
+        
+        return self.cache_data[key]
