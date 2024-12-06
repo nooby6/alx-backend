bee2f97cacdb1754e4d30fb86003d3046b6c78a4
Audrine Whrite <117525099+nooby6@users.noreply.github.com>
2024-12-06 16:54:25 +0300

    Fix merge conflict in Redis client connection handling

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,8 +2,4 @@ import { createClient } from 'redis';
 
 createClient()
   .on('error', (err) => console.log('Redis client not connected to the server:', err.stack))
-<<<<<<< HEAD
-  .on('ready', () => console.log('Redis client connected to the server'));
-=======
-  .on('ready', () => console.log('Redis client connected to the server'));
->>>>>>> a091f12e16d84bbdf6d2d9d2a11005351b870746
+.on('ready', () => console.log('Redis client connected to the server'));
